f8ceda70088b3dadd291e398ae5c6b407f77a872
pantorn <longpantorn@gmail.com>
2025-05-26 17:13:47 +0700

    finish part1

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import streamlit as st
 import streamlit_shadcn_ui as ui
+from dotenv import load_dotenv
+import os
+from huggingface_hub import InferenceClient
 
-trigger_btn = ui.button(text="Trigger Button", key="trigger_btn")
+import datetime
 
-ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an alert dialog", confirm_label="OK", cancel_label="Cancel", key="alert_dialog1")
+now = datetime.datetime.now()
 
-# st.title("Proficiency Voice Recognition")
-#
+# Format the date and time
+formatted_date_time = now.strftime('%Y/%m/%d %H:%M:%S')  
+
+print(formatted_date_time) 
+
+
+
+load_dotenv()
+
+token = os.getenv("HUGGINGFACE_TOKEN")
+
+
+st.title("Proficiency Voice Recognition")
+
+# receive voice
+audio_value = st.audio_input("Record a voice message")
+# if audio_value:
+#     st.audio(audio_value)
+
+
+
+# Save the audio bytes to a temporary file
+if audio_value:
+    with open("temp_audio.wav", "wb") as f:
+        f.write(audio_value.getvalue())
+
+    client = InferenceClient(
+        provider="fal-ai",
+        api_key=os.getenv("HUGGINGFACE_TOKEN"),
+    )
+
+    # ai
+    transcription = client.automatic_speech_recognition("temp_audio.wav", model="openai/whisper-large-v3")
+    
+    st.write("Transcription:")
+    st.write(transcription)
+
 # @st.dialog("Cast your vote")
 # def vote(item):
 #     st.write(f"Why is {item} your favorite?")
@@ -17,7 +55,7 @@ ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an 
 #
 # if "vote" not in st.session_state:
 #     st.write("Vote for your favorite")
-#     if st.button("A"):
+#     if st.button("Voice recognize"):
 #         vote("A")
 #     if st.button("B"):
 #         vote("B")
